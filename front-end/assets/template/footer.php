5218e72ae266e52d4bb42bba15c61a231c19b68a
Kassio <santoskassio562@gmail.com>
2026-05-10 01:47:58 -0300

    Upload das imagens do projeto, config de variaveis global, estruturas do projeto, finalização do footer

---
+++ front-end/assets/template/footer.php
@@ -0,0 +1,49 @@
+<footer>
+ <div class="footer-content">
+  <div class="contact_footer">
+   <div class="foo-logo">
+    <img src="<?= $BASE_URL ?>front-end/assets/img/informatica_logo_footer.png" alt="K7 Informática">
+   </div>
+
+   <div>
+    <div>
+     <p class="small">Tucuruví - São Paulo, SP</p>
+     <p class="small">Zona Norte - SP</p>
+    </div>
+    <div>
+     <p><span class="weak">Telefone:</span> +55 (11) 949214071</p>
+     <p><span class="weak">Email:</span> k7informatica@gmail.com</p>
+    </div>
+   </div>
+
+   <div class="foo-redes">
+    <span><img src="" alt=""></span>
+    <span><img src="" alt=""></span>
+   </div>
+  </div>
+
+  <div class="foo-links">
+   <h4>Links Úteis</h4>
+   <ul>
+    <li><img src="<?= $BASE_URL ?>front-end/assets/img/icons/arrow_icon.svg" alt=""><a href="<?= $BASE_URL ?>">Home</a></li>
+    <li><img src="<?= $BASE_URL ?>front-end/assets/img/icons/arrow_icon.svg" alt=""><a href="<?= $BASE_URL ?>">Serviços</a></li>
+    <li><img src="<?= $BASE_URL ?>front-end/assets/img/icons/arrow_icon.svg" alt=""><a href="<?= $BASE_URL ?>">Atendimento</a></li>
+    <li><img src="<?= $BASE_URL ?>front-end/assets/img/icons/arrow_icon.svg" alt=""><a href="<?= $BASE_URL ?>">Informações</a></li>
+   </ul>
+  </div>
+
+  <div class="foo-newsletter">
+   <h4>Newsletter</h4>
+   <p>Não iremos lotar sua caixa de entrada, apenas enviar dicas de como cuidar do seu PC ou Notebook.</p>
+   <form action="index.php" method="post">
+    <input type="email" name="email" id="emailInput" placeholder="Digite seu e-mail">
+    <button type="submit">Enviar</button>
+   </form>
+  </div>
+ </div>
+ <p class="small fooCopy">&copy; 2026 | Desenvolvido por K7 Sites - Todos os direitos reservados.</p>
+</footer>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require_once("./back-end/config/url.php");
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <!-- Google Fonts -->
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+ <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+ <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+ <!-- CSS do projeto -->
+ <link rel="stylesheet" href="./front-end/assets/css/main.css">
+ <title>K7 Informática</title>
+</head>
+
+<body>
+ <div class="container">
+  <!--  HEADER -->
+  <?php require_once("./front-end/assets/template/header.php"); ?>
+
+
+  <!-- MAIN -->
+
+
+
+  <!-- FOOTER -->
+  <?php require_once("./front-end/assets/template/footer.php"); ?>
+
+ </div>
