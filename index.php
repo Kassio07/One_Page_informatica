ee8d766844facd5dbb5c8ab0647e8b26d7228284
Kassio <santoskassio562@gmail.com>
2026-05-17 00:07:37 -0300

    implementa novas seções e padroniza paleta global do layout

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ require_once("./back-end/config/url.php");
 
  <!-- MAIN -->
  <main>
-  <!-- hero -->
+  <!-- Hiro -->
   <section class="hero" id="hero">
    <div class="container">
     <div class="hero-content">
@@ -84,16 +84,277 @@ require_once("./back-end/config/url.php");
   </section>
 
   <!-- Informação -->
-  <section class="section">
+  <section class="section-info">
    <div class="container">
     <div class="content">
      <div class="title">
       <h1>Seu <span style="color: var(--surface-color);">Notebook</span> ou <span style="color: var(--surface-color);">PC</span> funcionando <br> do jeito certo</h1>
       <p>Se você precisa de um desses serviços, entre em contato agora mesmo.</p>
      </div>
+     <div class="info-container">
+      <div class="info-content">
+       <div>
+        <div class="info-play">Vídeo</div>
+        <div class="info-text">
+         <span class="info-destaque"><img src="<?= $BASE_URL ?>front-end/assets/img/icons/Laptop Coding.svg" alt="">Notebook - <span class="accent-color">Assista o Vídeo</span></span>
+         <h2 class="h4">Montagem e Upgrade de <span class="surface-color">Notebook</span></h2>
+         <p>Instalações de peças, otimização e configuração para deixar seu notebook mais rápido, eficiente e pronto para qualquer tarefa.</p>
+         <a href="" class="btn-contato-info"><img src="<?= $BASE_URL ?>front-end/assets/img/icons/phone-etapas.png" alt="">Entre em contato</a>
+        </div>
+       </div>
+      </div>
+
+
+      <div class="info-content">
+       <div class="reverse-col">
+        <div class="info-play">Vídeo</div>
+        <div class="info-text">
+         <span class="info-destaque"><img src="<?= $BASE_URL ?>front-end/assets/img/icons/pc-gamer.svg" alt="">PC Gamer - <span class="accent-color">Assista o Vídeo</span></span>
+         <h2 class="h4">Montagem e Upgrade de <span class="surface-color">PC Gamer</span></h2>
+         <p>Instalações de peças, otimização e configuração para deixar seu notebook mais rápido, eficiente e pronto para qualquer tarefa.</p>
+         <a href="" class="btn-contato-info"><img src="<?= $BASE_URL ?>front-end/assets/img/icons/phone-etapas.png" alt="">Entre em contato</a>
+        </div>
+       </div>
+      </div>
+     </div>
     </div>
    </div>
   </section>
+
+  <!-- DIVISOR DE PÁGINA -->
+  <div class="divisor-pag">
+   <img src="<?= $BASE_URL ?>front-end/assets/img/divisor/divisor-to.png" alt="">
+  </div>
+  <!--/DIVIDOR DE PÁGINA -->
+
+  <!-- SERVIÇOS -->
+  <section class="servico">
+   <div class="container">
+    <div class="content">
+     <div class="title">
+      <h1>Conheça nossos serviços</h1>
+      <p>Serviços rápidos para pessoa física</p>
+     </div>
+     <div class="container-servico">
+      <div class="serv-pc">
+       <h3>Montagem de <span class="surface-color">PC Gamer</span></h3>
+       <p>Seu PC Gamer montando com precisão e alta performance</p>
+       <div class="service-img">
+        <img src="<?= $BASE_URL ?>front-end/assets/img/pc-gamer-montagem.png" alt="">
+       </div>
+      </div>
+      <div class="serv-notebook">
+       <h3>Manutenção de <span class="accent-color">Notebook</span></h3>
+       <p>Deixe seu Notebook mais rápido, seguro e funcionando como novo</p>
+       <div class="service-img">
+        <img src="<?= $BASE_URL ?>front-end/assets/img/manutencao-de-notebook.png" alt="">
+       </div>
+      </div>
+      <div class="serv-suporte">
+       <h3>Suporte <span class="surface-color">Remoto</span></h3>
+       <p>Atendimento remoto ágil para resolver seu problema na hora
+       </p>
+       <div class="service-img">
+        <img src="<?= $BASE_URL ?>front-end/assets/img/suporte-remoto.png" alt="">
+       </div>
+      </div>
+     </div>
+     <div class="service-itens-container">
+      <div class="service-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/Edit Pencil.svg" alt=""> Troca de pasta térmica
+      </div>
+      <div class="service-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/iMac.svg" alt=""> Troca de Tela
+      </div>
+      <div class="service-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/SSD.svg" alt=""> Instalação de SSD
+      </div>
+      <div class="service-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/Micro SD.svg" alt=""> Instalação de memória
+      </div>
+      <div class="service-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/Windows 8.svg" alt=""> Formatação
+      </div>
+      <div class="service-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/backup.svg" alt="Icone de Backup"> Backup
+      </div>
+      <div class="service-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/limpeza-notebook.svg" alt="Icone de Backup"> Limpeza
+      </div>
+     </div>
+     <div class="service-text">
+      <p><b>Oferecemos soluções completas para o seu notebook</b>, desde formatação, upgrades e troca de peças até manutenção preventiva e corretiva. Trabalhamos com agilidade, transparência e foco em desempenho, garantindo que seu equipamento funcione com máxima eficiência. Seja qual for o problema, nós temos a solução certa para você.</p>
+     </div>
+    </div>
+   </div>
+  </section>
+  <!-- /SERVIÇOS -->
+  <!-- DIVISOR DE PÁGINA -->
+  <div class="divisor-pag">
+   <img src="<?= $BASE_URL ?>front-end/assets/img/divisor/divisor-up.png" alt="">
+  </div>
+  <!--/DIVIDOR DE PÁGINA -->
+
+  <!-- BENEFICIOS / VANTAGENS -->
+  <section class="beneficios">
+   <div class="container">
+    <div class="content">
+     <div class="title">
+      <h1>Benefícios <b class="surface-color">/</b> Vantagens</h1>
+      <p>Confira os benefícios e vantagens de contratar nossos serviços</p>
+     </div>
+     <div class="beneficios-container">
+      <div class="beneficios-item">
+       <span><img src="<?= $BASE_URL ?>front-end/assets/img/icons/suporte-remoto.svg" alt=""></span>
+       <p>Atendimento rápido e personalizado</p>
+      </div>
+      <div class="beneficios-item">
+       <span><img src="<?= $BASE_URL ?>front-end/assets/img/icons/garantia.svg" alt=""></span>
+       <p>Produtos com procedência e garantia</p>
+      </div>
+      <div class="beneficios-item">
+       <span><img src="<?= $BASE_URL ?>front-end/assets/img/icons/equipe-info.svg" alt=""></span>
+       <p>Equipe com anos de experiência</p>
+      </div>
+      <div class="beneficios-item">
+       <span><img src="<?= $BASE_URL ?>front-end/assets/img/icons/suporte-tecnico.svg" alt=""></span>
+       <p>Suporte técnico completo e confiável</p>
+      </div>
+      <div class="beneficios-item">
+       <span><img src="<?= $BASE_URL ?>front-end/assets/img/icons/equipamentos-informatica.svg" alt=""></span>
+       <p>Variedade de equipamentos e acessórios</p>
+      </div>
+      <div class="beneficios-item">
+       <span><img src="<?= $BASE_URL ?>front-end/assets/img/icons/empresa-suporte.svg" alt=""></span>
+       <p>Solução para casa oyu empresa</p>
+      </div>
+
+     </div>
+
+    </div>
+   </div>
+  </section>
+
+  <!-- DIVISOR DE PÁGINA -->
+  <div class="divisor-pag">
+   <img src="<?= $BASE_URL ?>front-end/assets/img/divisor/divisor-to.png" alt="Divisor de página">
+  </div>
+  <!--/DIVIDOR DE PÁGINA -->
+
+  <!-- Faixa 1 - detalhes -->
+  <section class="faixa-detalhes">
+   <div class="container">
+    <div class="content">
+     <div class="details-img">
+      <img src="<?= $BASE_URL ?>front-end/assets/img/instalacao-ssd-notebook.svg" alt="Imagem de notebook junto com ssd">
+     </div>
+     <div class="details-text">
+      <p>Notebook Lento? Notebook travando? Não acessa sistema?
+      </p>
+      <h3 class="accent-color">Faça um Upgrade com SSD</h3>
+      <p>Colocar um SSD no seu notebook ou computador é uma das melhores melhorias que você pode fazer. Ele deixa o sistema muito mais rápido, reduz o tempo de inicialização e faz programas abrirem quase instantaneamente.</p>
+      <div class="details-btn">
+       <a href=""><img src="<?= $BASE_URL ?>front-end/assets/img/icons/Phone.svg" alt="">Faça um orçamento</a>
+      </div>
+     </div>
+    </div>
+   </div>
+  </section>
+  <!-- DIVISOR DE PÁGINA -->
+  <div class="divisor-pag">
+   <img src="<?= $BASE_URL ?>front-end/assets/img/divisor/divisor-up.png" alt="">
+  </div>
+  <!--/DIVIDOR DE PÁGINA -->
+
+  <!-- PROCURA -->
+  <section class="procura">
+   <div class="container">
+    <div class="content">
+     <div class="title">
+      <h1>Qual tipo de atendimento você está procurando?</h1>
+     </div>
+     <div class="container-procura">
+      <div class="procura-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/pc-conserto.svg" alt="">
+       <a href=""><img src="<?= $BASE_URL ?>front-end/assets/img/icons/whatsApp-logo.svg" alt="">Atendimento <br> Computador</a>
+      </div>
+      <div class="procura-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/Laptop.svg" alt="">
+       <a href=""><img src="<?= $BASE_URL ?>front-end/assets/img/icons/whatsApp-logo.svg" alt="">Atendimento <br> Macbook</a>
+      </div>
+      <div class="procura-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/Laptop-1.svg" alt="">
+       <a href=""><img src="<?= $BASE_URL ?>front-end/assets/img/icons/whatsApp-logo.svg" alt="">Atendimento <br> Notebook</a>
+      </div>
+      <div class="procura-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/imac-conserto.svg" alt="">
+       <a href=""><img src="<?= $BASE_URL ?>front-end/assets/img/icons/whatsApp-logo.svg" alt="">Atendimento <br> Imac</a>
+      </div>
+      <div class="procura-item">
+       <img src="<?= $BASE_URL ?>front-end/assets/img/icons/atendimento-informatica.svg" alt="">
+       <a href=""><img src="<?= $BASE_URL ?>front-end/assets/img/icons/whatsApp-logo.svg" alt="">Atendimento <br> Personalizado</a>
+      </div>
+     </div>
+    </div>
+   </div>
+  </section>
+
+  <!-- DIVISOR DE PÁGINA -->
+  <div class="divisor-pag">
+   <img src="<?= $BASE_URL ?>front-end/assets/img/divisor/divisor-to.png" alt="Divisor de página">
+  </div>
+  <!--/DIVIDOR DE PÁGINA -->
+
+  <!-- Faixa 2 - detalhes -->
+  <section class="faixa-detalhes">
+   <div class="container">
+    <div class="content">
+     <div class="details-img">
+      <!-- <img src="<?= $BASE_URL ?>front-end/assets/img/instalacao-ssd-notebook.svg" alt="Imagem de notebook junto com ssd"> -->
+      <h4>Etapas para <br> reparar <br> seu <span class="accent-color">Notebook</span></h4>
+      <p>Veja como e fácil o processo</p>
+     </div>
+     <div class="details-text">
+      <!-- <p>Notebook Lento? Notebook travando? Não acessa sistema?
+      </p>
+      <h3 class="accent-color">Faça um Upgrade com SSD</h3>
+      <p>Colocar um SSD no seu notebook ou computador é uma das melhores melhorias que você pode fazer. Ele deixa o sistema muito mais rápido, reduz o tempo de inicialização e faz programas abrirem quase instantaneamente.</p>
+      <div class="details-btn">
+       <a href=""><img src="<?= $BASE_URL ?>front-end/assets/img/icons/Phone.svg" alt="">Faça um orçamento</a>
+      </div> -->
+
+      <div class="details-passo-passo">
+       <div>
+        <span>1</span>
+        <div class="arrow"><img src="<?= $BASE_URL ?>front-end/assets/img/icons/arrow.svg" alt=""></div>
+        <img src="<?= $BASE_URL ?>front-end/assets/img/icons/etapa1.svg" alt="">
+        <p>Ligue para (11) 94921-4071</p>
+        <p>Apresenta o defeito e o problema</p>
+       </div>
+       <div> <span>2</span>
+        <div class="arrow"><img src="<?= $BASE_URL ?>front-end/assets/img/icons/arrow.svg" alt=""></div><img src="<?= $BASE_URL ?>front-end/assets/img/icons/notebook-iMac.svg" alt="">
+        <p>Entregue seu Notebook</p>
+        <p>Traga em mãos ou solicite a retirada</p>
+       </div>
+       <div> <span>3</span><img src="<?= $BASE_URL ?>front-end/assets/img/icons/entrega-notebook.svg" alt="">
+        <p>Aguarde o Conserto</p>
+        <p>Aprove o orçamento e aguarde conserto</p>
+       </div>
+
+      </div>
+     </div>
+    </div>
+   </div>
+  </section>
+  <!-- /Faixa 2 - detalhes -->
+  <!-- DIVISOR DE PÁGINA -->
+  <div class="divisor-pag">
+   <img src="<?= $BASE_URL ?>front-end/assets/img/divisor/divisor-up.png" alt="">
+  </div>
+  <!--/DIVIDOR DE PÁGINA -->
+
+
+
  </main>
 
 
